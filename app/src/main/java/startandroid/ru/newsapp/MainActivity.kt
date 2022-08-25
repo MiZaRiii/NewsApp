195a133a72324fc92790267379f7209cdc2871bc
MiZaRi <artemixerixer@gmail.com>
2022-08-26 01:54:45 +0700

    add try catch block

--- app/src/main/java/startandroid/ru/newsapp/MainActivity.kt
+++ app/src/main/java/startandroid/ru/newsapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package startandroid.ru.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        try {
+            println("3122")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
